b2fc668d1d61f394db99a0c23abd56caaa54ee02
Oleg Bobok <bobizma@gmail.com>
2013-12-01 00:04:25 +0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bsmal::Application.config.secret_key_base = 'a2c5a05a51127d242962ed1aefb4e66457f2b8e6227720e5181b8fb10c8718d2b3f4eb0d06e4426c7f676e0af45749b4c98a127b2c364e9b139ab5662091d810'
