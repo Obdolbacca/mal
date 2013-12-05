a53d1b8af70ec50b6fbf7e3d9b167e83bba35fbb
Oleg Bobok <bobizma@gmail.com>
2013-12-05 18:03:35 +0400

    i18n config fixed

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,9 @@ module Bsmal
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.enforce_available_locales = true
     config.i18n.default_locale = :ru
+
     config.sass.preferred_syntax = :sass
 
     config.time_zone = 'Europe/Moscow'
